454b90692af197a1b11ce531ee8b0e2ead7e29b8
May Thazin Khin <maythazinkhin53747@gmail.com>
2021-02-04 01:03:21 +0630

    first phase fianl

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,9 +12,6 @@ class DatabaseSeeder extends Seeder
             TableSeeder::class,
             CategorySeeder::class,
             ItemSeeder::class,
-            PrimarySeeder::class,
-            SecondarySeeder::class,
-            TertiarySeeder::class,
             SessionSeeder::class
         ]);
 
