f9f2778fd983331081834b7d45f8bffbcbde4a38
May Thazin Khin <maythazinkhin53747@gmail.com>
2021-03-01 14:28:50 +0630

    seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -8,6 +8,59 @@ use Illuminate\Database\Seeder;
 class CategorySeeder extends Seeder
 {
     public function run(){
-       Category::factory(20)->create();
+       //Category::factory(20)->create();
+        Category::create([
+            'name' => 'chalk',
+            'type_id' => 1
+        ]);
+        Category::create([
+            'name' => 'tip',
+            'type_id' => 1
+        ]);
+        Category::create([
+            'name' => 'salad',
+            'type_id' => 2
+        ]);
+        Category::create([
+            'name' => 'soup',
+            'type_id' => 2
+        ]);
+        Category::create([
+            'name' => 'fast food',
+            'type_id' => 2
+        ]);
+        Category::create([
+            'name' => 'fresh juice',
+            'type_id' => 3
+        ]);
+        Category::create([
+            'name' => 'mocktail',
+            'type_id' => 3
+        ]);
+        Category::create([
+            'name' => 'shisha',
+            'type_id' => 3
+        ]);
+        Category::create([
+            'name' => 'beer',
+            'type_id' => 3
+        ]);
+        Category::create([
+            'name' => 'energy drink',
+            'type_id' => 3
+        ]);
+        Category::create([
+            'name' => 'coffee',
+            'type_id' => 3
+        ]);
+        Category::create([
+            'name' => 'milk shake',
+            'type_id' => 3
+        ]);
+        Category::create([
+            'name' => 'smoothies',
+            'type_id' => 3
+        ]);
+
     }
 }
