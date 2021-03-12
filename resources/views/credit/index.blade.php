101f72099a629900472c0aad893e9b8d759e9ac9
nevarnar <loverhein2011@gmail.com>
2021-03-12 13:59:16 +0630

    login & credit

--- resources/views/credit/index.blade.php
+++ resources/views/credit/index.blade.php
@@ -7,13 +7,39 @@
             <table class="table table-borderless" id="myTable">
                 <thead>
                 <tr class="" style="border-bottom: 2px solid #dee2e6">
-                    <th class="table-header font-weight-normal">Date</th>
+                    <th class="table-header font-weight-normal">No</th>
                     <th class="table-header font-weight-normal">Member Name</th>
                     <th class="table-header font-weight-normal">Credit</th>
                     <th class="table-header font-weight-normal"> &nbsp;</th>
                 </tr>
                 </thead>
                 <tbody>
+                <tr>
+                    <td class="padding-table-row">
+                        <div class="text-td text-capitalize">
+                            1
+                        </div>
+                    </td>
+
+                    <td class="padding-table-row">
+                        <div class="text-td text-capitalize">
+                            name
+                        </div>
+                    </td>
+
+                    <td class="padding-table-row">
+                        <div class="text-td text-capitalize">
+                            1500
+                        </div>
+                    </td>
+
+                    <td class="padding-table-row w88px">
+                        <button type="button" class="btn-clear " title="Edit"  id="edit-button"
+                                data-toggle="modal" data-target="#modal">
+                            <i class="far fa-file-edit fw300" style="color:#673ab7;"></i>
+                        </button>
+                    </td>
+                </tr>
                 @foreach($invoices as $i=>$invoice)
                     <tr>
                         <td class="padding-table-row">
@@ -35,10 +61,9 @@
                         </td>
 
                         <td class="padding-table-row w88px">
-                            <button type="button" class="btn-clear " title="Edit"  id="edit-button">
-                                <a class="a-clear" href="{{route('invoice.detail',['id' => $invoice->id])}}">
+                            <button type="button" class="btn-clear " title="Edit"  id="edit-button"
+                                    data-toggle="modal" data-target="#modal">
                                     <i class="far fa-file-edit fw300" style="color:#673ab7;"></i>
-                                </a>
                             </button>
                         </td>
                     </tr>
@@ -48,6 +73,29 @@
             </table>
         </form>
     </div>
-
+    <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="add" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" style="width: 400px;">
+            <div class="modal-content" style="border-radius: 4px;" id="app">
+                <form id="add_form" action="{{route('staffs.store')}}"  method="post">
+                    @csrf
+                    <div class="modal-header border-bottom-0 mt-3">
+                        <div class="text-left pl-4 pt-1">
+                            <p style="font-family: 'Roboto',sans-serif;font-size: 18px!important;" class="fs18 mb-0 font-weight-bold">ပေးချေငွေ</p>
+                        </div>
+                    </div>
+                    <div class="modal-body mx-4 pt-2 pb-0">
+                        <div class="mb-3">
+                            <label for="name" class="label-form mb-1" style="font-size: 16px!important;">ပေးချေငွေ</label>
+                            <input type="text" id="name" name="name" class="input-form" placeholder="ပေးချေငွေထည့်ရန်" style="font-size: 15px!important;">
+                        </div>
+                    </div>
+                    <div class="modal-footer border-0 justify-content-between mx-3 px-4 mb-2 mt-4">
+                        <button type="button" class="btn pr-0" data-dismiss="modal" style="font-size: 16px!important;">မလုပ်တော့ပါ</button>
+                        <button type="submit" class="btn btn-info pl-3" style="font-size: 16px!important;" id="confirm-add-button"> Submit </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 @endsection
 
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -22,6 +22,12 @@
     <script src="{{asset('js/bootstrap.js')}}"></script>
     <script src="{{asset('js/bs.js')}}"></script>
 
+
+    <link rel="stylesheet" href="{{asset('css/login.css')}}">
+
+    <script src="{{asset('js/password_show.js')}}"></script>
+    <script src="{{asset('js/form_validation_msg.js')}}"></script>
+
     <script src="{{asset('js/MonthPicker.min.js') }}"> </script>
     <script src="{{asset('js/datepicker.min.js')}}"></script>
     <script src="{{ asset('js/yearpicker.js') }}"> </script>
@@ -47,22 +53,71 @@
         }
     </style>
 </head>
-<body style="background-color: #fff">
-<div class="overall-container-login">
-    <div class="main-container-login">
-        <div class="d-flex mx-auto" style="width: 400px">
-            <form class="position-relative w-100 h-100 bg-white p-3 mt-3" action="{{route('staff_login')}}"  method="post">
-                @csrf
-                <input type="text" name="name" id="phone" class="input-form" placeholder="Phone" style="font-size: 14px!important;">
-                <span class="text-danger">{{$errors->first('name')}}</span>
-                <input type="password"  name="password" id="password" class="input-form" placeholder="Password" style="font-size: 14px!important;">
-                <span class="text-danger">{{$errors->first('password')}}</span>
-                <button type="submit" class="btn btn-danger px-3 w-100 text-center">Login</button>
-            </form>
-        </div>
+<body class="bg-danger">
+{{--<div class="overall-container-login">--}}
+{{--    <div class="main-container-login">--}}
+{{--        <div class="d-flex mx-auto" style="width: 400px">--}}
+{{--            <form class="position-relative w-100 h-100 bg-white p-3 mt-3"   method="post">--}}
+{{--                @csrf--}}
+{{--                <input type="text" name="name" id="phone" class="input-form" placeholder="Phone" style="font-size: 14px!important;">--}}
+{{--                <span class="text-danger">{{$errors->first('name')}}</span>--}}
+{{--                <input type="password"  name="password" id="password" class="input-form" placeholder="Password" style="font-size: 14px!important;">--}}
+{{--                <span class="text-danger">{{$errors->first('password')}}</span>--}}
+{{--                <button type="submit" class="btn btn-danger px-3 w-100 text-center">Login</button>--}}
+{{--            </form>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--</div>--}}
+
+<div class="py-4 login-container">
+<div class="login-inner-container">
+
+    <div class="card-body pt-0 px-0">
+        <form method="POST" action="{{route('staff_login')}}">
+            @csrf
+            <div class="form-group text-center">
+                <i class="fas fa-user-circle text-danger" style="font-size: 120px;"></i>
+            </div>
+            <div class="form-group text-center">
+                <label for="phone" class="label-form">Username</label>
+                <div>
+                    <input type="text" name="name" id="phone" class="input-form" required placeholder="Phone" style="font-size: 14px!important;">
+                    <span class="text-danger">{{$errors->first('name')}}</span>
+                </div>
+            </div>
+            <div class="form-group text-center">
+                <label for="password" class="label-form">Password</label>
+
+                <div class="">
+                    <input type="password"  name="password" id="password" class="input-form" style="font-size: 14px!important;">
+                    <span class="text-danger">{{$errors->first('password')}}</span>
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <div class="col pl-5">
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                        <label class="form-check-label" for="remember">
+                            Remember Me
+                        </label>
+                    </div>
+                </div>
+            </div>
+            <div class="form-group row mb-0">
+                <div class="col">
+                    <button type="submit" class="btn btn-danger px-3 w-100 text-center" style="border-radius: 32px;">Login</button>
+                </div>
+            </div>
+
+
+
+
+        </form>
     </div>
 </div>
-
+</div>
 
 </body>
 </html>
